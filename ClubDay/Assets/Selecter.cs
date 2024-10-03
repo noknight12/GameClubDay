452aeea183fefa70b9257aba64730b354e8c6142
LordMangoz <umangabajgai@gmail.com>
2024-10-03 03:00:58 -0700

    let me sleep you rezero addict

--- ClubDay/Assets/Selecter.cs
+++ ClubDay/Assets/Selecter.cs
@@ -21,60 +21,105 @@ public class Selecter : MonoBehaviour
 
     public UIManager manager;
 
-    int charCount;
-    int abilityCount;
-    int enemyCount;
+    //int charCount;
+    //int abilityCount;
+    //int enemyCount;
+
     int count;
     int maxCount;
 
-    public Card[] characters;
+
+    Card selectedCard;
+
+
+    public Card[] cards;
 
     // Start is called before the first frame update
     void Start()
     {
-        charCount = charGroup.cards.Length;
-        
+        currentGroup = charGroup;
+        maxCount = currentGroup.cards.Length;
+        cards = currentGroup.cards;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) && count <= maxCount && count >= 0)
+        //arrow keys through the cards
+        if (Input.GetKeyDown(KeyCode.RightArrow) && count < maxCount - 1 && count >= 0)
         {
+
             count++;
-         
+            Debug.Log(count);
+
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && count <= maxCount && count >= 0)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && count < maxCount && count > 0)
         {
             count--;
-            
+            Debug.Log(count);
         }
 
 
-       
+        // Card Selected
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SelectCard();
+            ChangeStage(selectStage + 1);
+        }
+
+
     }
 
+
+    void SelectCard()
+    {
+        selectedCard = cards[count]; // simply sets the card selected to be the current card we are at rn
+        Debug.Log(selectedCard);
+    }
+
+
+    // picks if the curent stage is a character card, abililty or a enemy
     void ChangeStage(int targetStage)
     {
         selectStage = targetStage;
-
         if (selectStage == 1)
         {
+            //select char
             currentGroup = charGroup;
+
+            abilityGroup.enabled = false;
+            enemyGroup.enabled = false;
+            maxCount = currentGroup.cards.Length;
+            cards = currentGroup.cards;
         }
+
         else if (selectStage == 2)
         {
+            //select abiilty
             abilityGroup = selectedChar.abilityCards;
             currentGroup = abilityGroup;
-
+            charGroup.enabled = false;
+            enemyGroup.enabled = false;
+            maxCount = currentGroup.cards.Length;
+            cards = currentGroup.cards;
+            Debug.Log("Now selecting abilities.");
         }
         else if (selectStage == 3)
         {
+            //select target
             currentGroup = enemyGroup;
+            abilityGroup.enabled = false;
+            charGroup.enabled = false;
+            maxCount = currentGroup.cards.Length;
+            cards = currentGroup.cards;
+            Debug.Log("Now selecting enemies.");
         }
         else
         {
             Debug.LogError("wuh oh");
         }
+        count = 0;
     }
 }
+    //figure out to scroll throuhg abiilties list
+
