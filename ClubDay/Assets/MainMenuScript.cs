9ad331cbacefd531cfa32e0693a2c1c6162fd30d
LordMangoz <umangabajgai@gmail.com>
2024-10-03 04:34:23 -0700

    changes
    im working late cuz im a singerr

---
+++ ClubDay/Assets/MainMenuScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playmusic
+        //have the click to start game button script running 
+        //if game is starteedd the swap from main menu music to boss music 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
