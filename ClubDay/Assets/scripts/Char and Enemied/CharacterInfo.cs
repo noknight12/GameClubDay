30340418ba1e8b380c96933ffb5d36d081f0ac68
LordMangoz <umangabajgai@gmail.com>
2024-10-04 05:48:30 -0700

    end my suffering my chest hurts

--- ClubDay/Assets/scripts/Char and Enemied/CharacterInfo.cs
+++ ClubDay/Assets/scripts/Char and Enemied/CharacterInfo.cs
@@ -9,7 +9,7 @@ public class CharacterInfo : MonoBehaviour
     public int stamina;
     public int mana;
     public int numberOfAbilities;
-
+    public int MaxHealth;
     public bool isAlive;
 
     public bool isMage;
@@ -19,16 +19,61 @@ public class CharacterInfo : MonoBehaviour
     public CardGroup CardGroup;
     public Card characterCard;
     public CardGroup abilityCards;
+
+    public health_Bar health_bar;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = MaxHealth;
+        //health_Bar.SetMaxHealth(MaxHealth); // Set the health bar's max value
+        isAlive = true;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+            
     }
+    public void TakeDamage(int damage)
+    {
+        if (isAlive)
+        {
+            health -= damage; // Reduce health by the damage amount
+
+            // Check if health goes below 0 and set it to 0
+            if (health < 0)
+            {
+                health = 0;
+            }
+
+            health_bar.SetHealth(health); // Update the health bar
+
+            if (health <= 0)
+            {
+                isAlive = false; // Mark character as dead
+                // Handle character death (e.g., disable the character, trigger animations, etc.)
+            }
+        }
+    }
+
+    // Function to heal the character
+    public void Heal(int healAmount)
+    {
+        if (isAlive)
+        {
+            health += healAmount; // Increase health by the heal amount
+
+            // Check if health exceeds max health and set it to MaxHealth
+            if (health > MaxHealth)
+            {
+                health = MaxHealth;
+            }
+
+            health_bar.SetHealth(health); // Update the health bar
+        }
+    }
+
+
 }
